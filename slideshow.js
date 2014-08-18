1dddf4e0b1bc6601efaf534f18764cc2dacb3571
radicaldude <joe.shutts@gmx.co.uk>
2014-08-18 14:57:04 +0100

    jquery slideshow

---
+++ slideshow.js
@@ -0,0 +1,25 @@
+var path = ["image1.jpg","image2.jpg","image3.jpg"];
+var i;
+function swapImage(){
+slide = document.images.slide;
+$(slide).fadeOut(50);
+setTimeout("swapAnimate()", 100)
+setTimeout("swapImage()",5000);
+}
+function swapAnimate(){
+$(slide).animate({
+			
+				left:'+=100px',	
+				  height:'482.375px',
+				  width:'703.125px'});
+document.images.slide.src=path[i];
+$(slide).fadeIn();
+$(slide).animate({
+			
+				left:'-=100px',
+			
+				  height:'578.85px',
+				  width:'843.75px'}, 3000);
+if (i<path.length-1) i++; else i =0;
+}
+window.onload=swapImage;
